6e89f1bfd38cf66a75e7c8bb67e7ad648a4125ba
逸鴻 張 <winterer32@outlook.com>
2022-04-06 11:29:23 +0800

    更新資料驗證屬性

--- DIY_v2/Models/Tasker.cs
+++ DIY_v2/Models/Tasker.cs
@@ -11,6 +11,8 @@ namespace DIY_v2.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
     
     public partial class Tasker
     {
@@ -21,21 +23,63 @@ namespace DIY_v2.Models
             this.TaskerService = new HashSet<TaskerService>();
         }
     
+        [DisplayName("師傅編號")]
         public int TaskerID { get; set; }
+
+        [Required(ErrorMessage = "請填寫姓名")]
+        [DisplayName("師傅姓名")]
         public string TaskerName { get; set; }
+
+        [Required(ErrorMessage = "請填寫每趟的服務報價")]
+        [Range(1, 30000, ErrorMessage = "報價必須介於1~30000")]
+        [DisplayName("服務報價/趟")]
         public short ServiceQuote { get; set; }
+
+        [Required(ErrorMessage = "請填寫連絡電話")]
+        [DisplayName("連絡電話")]
         public string TaskerTel { get; set; }
+
+        [Required(ErrorMessage = "請填寫師傅手機")]
+        [DisplayName("師傅手機")]
         public string TaskerPhone { get; set; }
+
+        [Required(ErrorMessage = "請填寫營業時間")]
+        [DisplayName("營業時間")]
         public System.TimeSpan ServiceTime_Start { get; set; }
+
+        [Required(ErrorMessage = "請填寫打烊時間")]
+        [DisplayName("打烊時間")]
         public System.TimeSpan ServiceTime_End { get; set; }
+
+        [Required(ErrorMessage = "請填寫服務地區")]
+        [DisplayName("服務地區")]
         public string ServiceArea { get; set; }
+
+        [DisplayName("師傅頭像")]
         public string TaskerImage { get; set; }
+
+        [DisplayName("專業證照")]
         public string License { get; set; }
+
+        [DisplayName("特色")]
+        [StringLength(500, ErrorMessage = "特色內容請控制在50個字以內")]
         public string Feature { get; set; }
+
+        [DisplayName("服務內容")]
+        [StringLength(500, ErrorMessage = "服務內容請控制在500個字以內")]
         public string TaskerDescription { get; set; }
+
+        [DisplayName("施工案例")]
         public string CaseImage { get; set; }
+
+        [DisplayName("評價")]
         public Nullable<decimal> Rate { get; set; }
+
+        [Required]
         public int MemberID { get; set; }
+
+        [DisplayName("權限")]
+        [Required]
         public string Permission { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
