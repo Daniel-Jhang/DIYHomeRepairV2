21134a7fb99ff043669658ed718c952413ec31b4
曜源 田 <yirker@hotmail.com>
2022-04-06 13:47:47 +0800

    GOOGLE地圖搜尋

---
+++ DIY_v2/Controllers/GoogleAPIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DIY_v2.Controllers
+{
+    public class GoogleAPIController : Controller
+    {
+        // GET: GoogleAPI
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
